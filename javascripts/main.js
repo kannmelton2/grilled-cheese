0f9fc0eb02beddf0c1fa3b8df71576081ca6fe36
Kayla Melton <kannmelton2@gmail.com>
2020-02-25 20:37:55 -0600

    radio buttons work

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('grilled cheese');
+// div in html - id="bread-container" - DONE
+// printToDom function in utils - DONE
+// breadData will have array of bread objects, export a getBreads function - DONE
+// each bread object = type, color, price, id - DONE
+// breads.js component - with domString builder (find something nice in bootstrap) use getBreads function - DONE
+// import breads component into main - DONE
+// event listener on radio button - call breadData.setSelectedBread(breadId);
+// setSelectedBread = assign a variable (selectedBreadId) to the id passed in (console.log the id)
+
+import breads from './components/breads.js';
+
+const init = () => {
+    breads.makeBreadRadioButtons();
+  };
+  
+  init();
